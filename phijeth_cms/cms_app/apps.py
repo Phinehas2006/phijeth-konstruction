7259107327074e6eb0e144001cef56a3a3388cad
Phinehas2006 <powohphinehas@gmail.com>
2026-06-04 23:45:48 +0100

    Ensure admin user on startup

--- phijeth_cms/cms_app/apps.py
+++ phijeth_cms/cms_app/apps.py
@@ -5,3 +5,28 @@ class CmsAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cms_app'
     verbose_name = 'PHIJETH CMS'
+
+    def ready(self):
+        import os
+        import sys
+
+        if 'runserver' not in sys.argv and 'gunicorn' not in sys.argv[0]:
+            return
+
+        username = os.getenv('DJANGO_SUPERUSER_USERNAME')
+        password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
+        email = os.getenv('DJANGO_SUPERUSER_EMAIL', '')
+        if not username or not password:
+            return
+
+        try:
+            from django.contrib.auth import get_user_model
+            User = get_user_model()
+            user, _created = User.objects.get_or_create(username=username)
+            user.email = email
+            user.is_staff = True
+            user.is_superuser = True
+            user.set_password(password)
+            user.save()
+        except Exception:
+            pass
